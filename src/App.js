1f3ffe213a15802b376757986c181bc58b9d02f6
ANAWARdev <143820904+ANAWARdev@users.noreply.github.com>
2024-06-21 11:43:23 +0200

    Changement du titre en cliquant sur le bouton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Title = ({ content }) => {
+  return <h1 id="title">{content}</h1>;
+};
+
+const Component = ({ title, onClick }) => (
+  <button onClick={() => onClick(title)}>Learn {title}</button>
+);
 
 function App() {
-  return (
+  const handleOnClick = (lib) =>
+    document.getElementById("title").innerText = lib;
+  
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title content=" Welcome !" />
+        <Component onClick={handleOnClick} title="React" />
+        <Component onClick={handleOnClick} title="Vue" />
+        <Component onClick={handleOnClick} title="Angular" />
       </header>
     </div>
   );
